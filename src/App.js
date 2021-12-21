f1124dcebd08984d8247977829933ce0cc2b640c
Mirai <delmontejohndaniel1415@gmail.com>
2021-12-21 19:29:29 +0800

    Using lifecycle hooks

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import ColorPicker from './ColorPicker';
-import { robots } from './robots';
 import './App.css';
+import { robots } from './robots';
 
 class App extends Component{
 
   constructor(){
     super();
     this.state = {
+      robots: [],
       searchField: '',
-      color1: '#000000',
+      color1: '#000000',  
       color2: '#ffffff'
     }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -30,19 +37,26 @@ class App extends Component{
   }
 
   render() {
-
     const gradientBackground = `linear-gradient(to right, ${this.state.color1}, ${this.state.color2})`
-    document.querySelector('body').style.background = gradientBackground;
-    
     const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()));
-    return(
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <ColorPicker inputChange={this.onInputChange} currentColor={gradientBackground} color1={this.state.color1} color2={this.state.color2}/>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    document.querySelector('body').style.background = gradientBackground;
+    if(this.state.robots.length === 0){
+      return <h1>Loading</h1>
+    }else{
+      return(
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <ColorPicker 
+            inputChange={this.onInputChange} 
+            currentColor={gradientBackground} 
+            color1={this.state.color1} 
+            color2={this.state.color2}
+          />
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
