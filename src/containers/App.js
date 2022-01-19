2cb88d6e9da385381248f0cf308579b3c2e27bea
Mirai <delmontejohndaniel1415@gmail.com>
2022-01-19 19:02:46 +0800

    Convert class based component into functional component using react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,61 +1,55 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ColorPicker from '../components/ColorPicker';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component{
+const App = () => {
 
-  state = {
-    robots: [],
-    searchField: '',
-    color1: '#000000',
-    color2: '#ffffff',
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
+  const [color1, setColor1] = useState('#000000');
+  const [color2, setColor2] = useState('#ffffff');
+  
+  function onInputChange(event) {
+    const { value } = event.target.attributes.id;
+    value === 'color1' ? setColor1(event.target.value) : setColor2(event.target.value);
   }
 
-  // Lifecycle methods (mounting)
-  componentDidMount(){
+  function onSearchChange(event) {
+    setSearchfield(event.target.value);
+  }
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
-  }
+      .then(users => setRobots(users))
+  }, []);
 
-  onSearchChange = (event) => {
-    // Changing the state
-    this.setState({ searchField: event.target.value });
-  };
+  const filteredRobots = robots.filter(robot => {
+    robot.name.toLowerCase().includes(searchfield.toLowerCase())
+  });
 
-  onInputChange = (event) => {
-    const { value } = event.target.attributes.id;
-    value === 'color1' ?
-      this.setState({ color1: event.target.value })
-      : this.setState({ color2: event.target.value });
+  const gradientBackground = `linear-gradient(to right, ${color1}, ${color2})`
+  document.querySelector('body').style.background = gradientBackground;
 
-  };
-
-  render() {
-    const {robots, searchField, color1, color2} = this.state;
-    const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
-    const gradientBackground = `linear-gradient(to right, ${color1}, ${color2})`
-    document.querySelector('body').style.background = gradientBackground;
-
-    return (!robots.length) ?
-      <h1>Loading</h1>
+  return !robots.length ?
+    <h1>Loading</h1>
       :<div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <ColorPicker 
-          inputChange={this.onInputChange} 
+          inputChange={onInputChange} 
           currentColor={gradientBackground} 
-          color1={this.state.color1} 
-          color2={this.state.color2}
+          color1={color1} 
+          color2={color2}
         />
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
         </Scroll>
+        {console.log(count)}
       </div>
-  }
 }
 
 export default App;
